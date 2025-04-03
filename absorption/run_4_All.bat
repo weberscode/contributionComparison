82534e66240601fa8a498f55bd893938cd5e2bde
weberscode <72982682+weberscode@users.noreply.github.com>
2025-04-03 11:11:49 +0200

    Example for simple shoebox

---
+++ absorption/run_4_All.bat
@@ -0,0 +1,16 @@
+echo ::Generating scene octree::
+oconv mat.rad geo.rad > scene.oct
+
+echo ::Running rfluxmtx for direct sun::
+rfluxmtx -c 100 -V+ -ab 0 -t 5 -dj 0 sender4.rad receiversAll.rad -i scene.oct > dirr.mtx
+
+echo ::Converting direct sun results to irradiance::
+rmtxop -faa -c 0.333333333 0.333333333 0.333333334 -t dirr.mtx > dirrFin.txt
+
+echo ::Running rfluxmtx for combined (diffuse + direct) sun simulations::
+rfluxmtx -c 100 -V+ -ab 6 -ad 25000 -lw 4.0000e-05 -t 5 -dj 0 sender4.rad receiversAll.rad -i scene.oct > tot.mtx
+
+echo ::Converting total sun results to irradiance::
+rmtxop -faa -c 0.333333333 0.333333333 0.333333334 -t tot.mtx > totFin.txt
+
+echo ::Simulation(s) complete::
